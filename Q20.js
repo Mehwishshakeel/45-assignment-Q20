b3e339c6113922071416cd4d8927378131b0b113
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-03-17 01:41:13 +0500

    Add files via upload

---
+++ Q20.js
@@ -0,0 +1,7 @@
+"use strict";
+// list of subjects
+let subjects = ["Maths", "English", "Science", "Computer"];
+// print in the form of list
+console.log("list of subjects");
+// in the form of list
+subjects.forEach(oneSubject => console.log(oneSubject));
---
+++ Q20.ts
@@ -0,0 +1,6 @@
+// list of subjects
+let subjects:string[]=["Maths","English","Science","Computer"];
+// print in the form of list
+console.log("list of subjects");
+// in the form of list
+subjects.forEach(oneSubject=>console.log(oneSubject));
